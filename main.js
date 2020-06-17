02f0e3ed7cba74d32c16ec06f22c91b3f26fe885
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-17 13:03:09 -0400

    .

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 // Your code here!
 
+//path to file
+const colorCombinator = require('./color-combinator.js')
+const colorDeconstructor = require('./color-deconstructor.js')
 
+
+//grab user input
+
+let input = getInput(0);
+let input2 = getInput(1);
+
+//  prints message stating if not given a value 
+if(input === undefined && input2 === undefined){
+  console.log("You need at least one value")
+}//prints colorCombinator message of both being two colors
+if(input !== undefined && input2 !== undefined){
+  console.log(colorCombinator(input,input2))
+}//prints colorDeconstructor message if only given one color (secondary)
+if(input !== undefined && input2 === undefined){
+  let decon = colorDeconstructor(input)
+  console.log(decon)
+}
+
+// if first input is undefined
+
+// noFirst = colorCombinator(input,input2)
+
+
+
+
+
+//print out the message
+// console.log(noFirst)
 
 // Our helper function here! Don't  touch!
 function getInput(i) {
